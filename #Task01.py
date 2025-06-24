b574c48da966cfc298d65c23efd443aba927fff0
Umme Habiba <saeedhabiba001@gmail.com>
2025-06-24 21:47:35 +0500

    Add files via upload

---
+++ #Task01.py
@@ -0,0 +1,11 @@
+#Task01
+#initialize first
+name = "Umme Habiba"
+hobby = "Designing Posters"
+age = 21
+reason = "I want to learn Python to build creative and smart applications."
+#now let's execute it
+print("Name: ", name)
+print("Hobby: ", hobby)
+print("Age: ", age)
+print("Why I want to learn Python: ", reason)
---
+++ Task02.py
@@ -0,0 +1,6 @@
+#let's add numbers
+#initialize first
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = num1 + num2
+print("The sum is:", result)
